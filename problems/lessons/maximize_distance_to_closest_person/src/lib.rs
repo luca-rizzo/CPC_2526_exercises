a02e3d8d05899c32b4b7e5ea42a4d55d4a06e524
Luca Rizzo <lucalucasrizzo@gmail.com>
2025-10-24 11:40:45 +0200

    better solution to maximum distance

--- problems/lessons/maximize_distance_to_closest_person/src/lib.rs
+++ problems/lessons/maximize_distance_to_closest_person/src/lib.rs
@@ -1,7 +1,6 @@
-struct Solution;
+struct BsSolution;
 
-impl Solution {
-
+impl BsSolution {
     // T(n) = O(n * log(n)) with n = n_seats because we perform a binary search on a range [0,..., n_seats - 1]
     // and every check has a complexity O(n) because we check all people sitting that are potentially n_seats
     pub fn max_dist_to_closest(seats: Vec<i32>) -> i32 {
@@ -56,6 +55,32 @@ impl Solution {
     }
 }
 
+struct LinearSolution;
+
+impl LinearSolution {
+    pub fn max_dist_to_closest(seats: Vec<i32>) -> i32 {
+        let index_people_sitting: Vec<usize> = seats
+            .iter()
+            .enumerate()
+            .filter(|&(_, v)| *v != 0)
+            .map(|(index, _)| index)
+            .collect();
+        let first_person_index = index_people_sitting[0];
+        let last_person_index = index_people_sitting.last().unwrap();
+        let opt_distance_b_person: Vec<usize> = index_people_sitting
+            .windows(2)
+            .map(|v| {
+                let middle = v[0] + (v[1] - v[0]) / 2;
+                middle - v[0]
+            })
+            .collect();
+        let max_between_people = *(opt_distance_b_person.iter().max().unwrap_or(&0)) as i32;
+
+        max_between_people
+            .max(first_person_index as i32)
+            .max((seats.len() - 1 - last_person_index) as i32)
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -64,12 +89,20 @@ mod tests {
     fn check_d2() {
         assert_eq!(
             2,
-            Solution::max_dist_to_closest(Vec::from([1, 0, 0, 0, 1, 0, 1]))
+            BsSolution::max_dist_to_closest(Vec::from([1, 0, 0, 0, 1, 0, 1]))
+        );
+        assert_eq!(
+            2,
+            LinearSolution::max_dist_to_closest(Vec::from([1, 0, 0, 0, 1, 0, 1]))
         );
     }
 
     #[test]
     fn check_one_people() {
-        assert_eq!(3, Solution::max_dist_to_closest(Vec::from([1, 0, 0, 0])));
+        assert_eq!(3, BsSolution::max_dist_to_closest(Vec::from([1, 0, 0, 0])));
+        assert_eq!(
+            3,
+            LinearSolution::max_dist_to_closest(Vec::from([1, 0, 0, 0]))
+        );
     }
 }
