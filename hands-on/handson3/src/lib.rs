0dae175e479cd5d2a667740d2fd5a5ed1f4f2a8b
luuukeeeee <lucalucasrizzo@gmail.com>
2025-12-06 10:34:35 +0100

    space optimized version of first exercise

--- hands-on/handson3/src/lib.rs
+++ hands-on/handson3/src/lib.rs
@@ -1,56 +1,44 @@
 pub mod holiday_planning {
-    // in cities_attrs[i][j] we find the attractions that you can visit in city i
-    // in day j. ex cities_attrs[0][3] = 4 -> you can see 4 attraction in day 4 in city 0
-
     use std::cmp::max;
     use std::error::Error;
+    use std::mem;
 
-    struct Item {
-        w: u32,
-        v: u32,
-    }
-
-    impl Item {
-        fn new(v: u32, w: u32) -> Self {
-            Self { w, v }
-        }
-    }
-
+    // in cities_attrs[i][j] we find the attractions that you can visit in city i
+    // in day j. ex cities_attrs[0][3] = 4 -> you can see 4 attraction in day 4 in city 0
     fn max_visitable_attractions(cities_attrs: Vec<Vec<u32>>) -> u32 {
         let n = cities_attrs.len();
         let d = cities_attrs.get(0).map(|c| c.len()).unwrap_or(0);
-        let mut items: Vec<Item> = Vec::with_capacity(n * d);
-        for city in cities_attrs {
-            let mut sum = 0;
-            for (idx, num_attr) in city.iter().enumerate() {
-                sum += num_attr;
-                items.push(Item::new(sum, (idx + 1) as u32));
-            }
-        }
-        let mut t: Vec<Vec<u32>> = vec![vec![0; d + 1]; n * d + 1];
-        for i in 1..(n * d + 1) {
-            let c_item = &items[i - 1];
-            for j in 1..(d + 1) {
-                if j < c_item.w as usize {
-                    t[i][j] = t[i - 1][j];
-                    continue;
+        let mut prev: Vec<u32> = vec![0u32; d + 1];
+        let mut curr: Vec<u32> = vec![0u32; d + 1];
+        for i in 1..(n + 1) {
+            let city = &cities_attrs[i - 1];
+            for av_days in 1..(d + 1) {
+                // let's initialize it with the choice of not consider this city in our trip
+                curr[av_days] = prev[av_days];
+                let mut cum_attrs = 0;
+                // For every feasible choice 'day' (1 ≤ day ≤ av_days) we consider the
+                // option of staying exactly 'day' days in the current city.
+                // This grants the cumulative attractions of its first 'day' entries.
+                // Because those 'day' days consume part of the total 'av_days',
+                // the remaining 'av_days - day' must be filled using the best solution
+                // computed for the previous cities (stored in 'prev').
+                for day in 1..(av_days + 1) {
+                    cum_attrs += city[day - 1];
+                    curr[av_days] = max(prev[av_days - day] + cum_attrs, curr[av_days])
                 }
-                t[i][j] = max(
-                    t[i - 1][j],
-                    t[i - c_item.w as usize][j - c_item.w as usize] + c_item.v,
-                );
             }
+            mem::swap(&mut curr, &mut prev);
         }
-        t[n * d][d]
+        prev[d]
     }
 
     pub fn solve(input: &str) -> Result<u32, Box<dyn Error>> {
         let mut iter = input.split_whitespace();
         let n: usize = iter.next().ok_or("missing n")?.parse()?;
-        let D: usize = iter.next().ok_or("missing D")?.parse()?;
+        let d: usize = iter.next().ok_or("missing D")?.parse()?;
         let arr: Vec<Vec<u32>> = (0..n)
             .map(|_| {
-                (0..D)
+                (0..d)
                     .map(|_| {
                         let s = iter.next().ok_or("missing value")?;
                         let v = s.parse::<u32>()?;
@@ -65,7 +53,7 @@ pub mod holiday_planning {
 
     #[cfg(test)]
     mod tests {
-        use crate::holiday_planning::{max_visitable_attractions, solve};
+        use crate::holiday_planning::solve;
 
         fn run_case(i: usize) -> Result<(), Box<dyn std::error::Error>> {
             let input =
@@ -101,6 +89,5 @@ pub mod holiday_planning {
         fn case4() -> Result<(), Box<dyn std::error::Error>> {
             run_case(4)
         }
-
     }
 }
